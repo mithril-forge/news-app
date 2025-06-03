e6e83e1bd52c47ee1cb6cc7a3b174e1641e3f8bc
Simon Foucek <sfoucek@redhat.com>
2025-06-03 22:50:00 +0200

    original behaviour

--- backend/news_processing.py
+++ backend/news_processing.py
@@ -53,9 +53,8 @@ async def generate_and_connect_news(delta: timedelta):
             archive=local_archive,
             ai_model=GeminiAIModel(api_key=gemini_api_key)
         )
-        # TODO: Fix connection and enable again -> problems when deleting tags
-        #await session.flush()
-        #await article_generation_service.connect_existing_news(delta=delta)
+        await session.flush()
+        await article_generation_service.connect_existing_news(delta=delta)
         await session.flush()
         await article_generation_service.creates_new_news(delta=delta)
 
